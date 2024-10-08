914864190c5a6391ed70d8da8c09438171203b34
FEDERICO <federicobuta51@gmail.com>
2024-10-07 22:00:17 -0300

    Cambio de color botones Home, se eliminaron cosas imports sin uso.

--- app/src/main/java/com/example/agromanager2_0/Home.java
+++ app/src/main/java/com/example/agromanager2_0/Home.java
@@ -6,11 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
